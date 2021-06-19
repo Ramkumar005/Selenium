79f3f6fc872deb0bf73123eee3d545ce71ce03de
Ramkumar005 <63731813+Ramkumar005@users.noreply.github.com>
2021-06-19 21:09:18 +0530

    Update OddOrEven.java

--- OddOrEven.java
+++ OddOrEven.java
@@ -3,7 +3,7 @@ package week1_Day1.Assignment;
 public class OddOrEven {
 
 	public static void main(String[] args) {
-
+                int num1= 0;
 		int num = 4;
 		if (num % 2 == 0) {
 			System.out.println("even number");
